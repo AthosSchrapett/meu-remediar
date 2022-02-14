c841de9fdd27cf18c049ae3385b2260090f79ebd
Athos Schrapett <athosschrapett@outlook.com>
2022-02-14 19:29:30 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./styles.css"
 
-function App() {
+export default function Frame60() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="frame-60 flex-col-hcenter">
+      <p className="txt-872 flex-hcenter">Medicamentos</p>
+      <div className="bloco flex-col-hcenter-vcenter"></div>
+      <div className="contents-01 flex-col-hcenter">
+        <div className="titulo flex-row-hsb">
+          <p className="txt-297">Perindopril</p>
+          <p className="txt-735 flex-hend">08:00</p>
+        </div>
+        <div className="group-15 flex-row-hsb">
+          <p className="txt-618">Lembre-me de tomar</p>
+          <div className="trigger">
+            <div className="ellipse-5" />
+          </div>
+        </div>
+        <p className="txt-991">Caixa com 60 comprimidos - 04mg</p>
+        <p className="txt-9102">1 comprimido a cada 4h</p>
+        <p className="txt-339">Atrasado!</p>
+        <div className="btn-primary">
+          <div className="btn-text-master">
+            <p className="text-master flex-vcenter-hcenter">
+              Confirmar consumo
+            </p>
+          </div>
+        </div>
+        <div className="btn-secondary">
+          <div className="btn-text-master-1">
+            <p className="text-master-1 flex-vcenter-hcenter">
+              Ver informações
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="contents-02 flex-col-hcenter">
+        <div className="titulo-1 flex-row-hsb">
+          <p className="perindopril">Losartana</p>
+          <p className="_-0800 flex-hend">10:00</p>
+        </div>
+        <div className="group-151 flex-row-hsb">
+          <p className="txt-992">Lembre-me de tomar</p>
+          <div className="trigger-1">
+            <div className="ellipse-51" />
+          </div>
+        </div>
+        <p className="caixa-com-60-comprimidos-04mg">
+          Caixa com 30 comprimidos - 50mg
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="_-1-comprimido-acada-4h">1 comprimido a cada 12h</p>
+        <div className="btn-primary-1">
+          <div className="btn-text-master-2">
+            <p className="text-master flex-vcenter-hcenter">
+              Confirmar consumo
+            </p>
+          </div>
+        </div>
+        <div className="btn-secondary-1">
+          <div className="btn-text-master-3">
+            <p className="text-master-1 flex-vcenter-hcenter">
+              Ver informações
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="contents-03 flex-col-hcenter">
+        <div className="titulo-2 flex-row-hsb">
+          <p className="txt-322">Perindopril</p>
+          <p className="_-0800 flex-hend">12:00</p>
+        </div>
+        <div className="group-152 flex-row-hsb">
+          <p className="txt-077">Lembre-me de tomar</p>
+          <div className="trigger-2">
+            <div className="ellipse-52" />
+          </div>
+        </div>
+        <p className="txt-160">Caixa com 60 comprimidos - 04mg</p>
+        <p className="txt-340">1 comprimido a cada 4h</p>
+        <div className="btn-primary-2">
+          <div className="btn-text-master-4">
+            <p className="text-master flex-vcenter-hcenter">
+              Confirmar consumo
+            </p>
+          </div>
+        </div>
+        <div className="btn-secondary-2">
+          <div className="btn-text-master-5">
+            <p className="text-master-1 flex-vcenter-hcenter">
+              Ver informações
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="contents-04 flex-col-hcenter">
+        <p className="txt-882">Próximos medicamentos</p>
+        <div className="frame-61 flex-col-hcenter-vcenter">
+          <div className="medicamento-2 flex-row-hsb">
+            <p className="txt-663">Perindopril - 04mg</p>
+            <p className="txt-9105 flex-hend">8:00</p>
+          </div>
+          <div className="medicamento-1 flex-row-hsb">
+            <p className="txt-1097">Perindopril - 04mg</p>
+            <p className="txt-684 flex-hend">12:00</p>
+          </div>
+          <div className="medicamento-3 flex-row-hsb">
+            <p className="txt-783">Perindopril - 04mg</p>
+            <p className="txt-446 flex-hend">16:00</p>
+          </div>
+        </div>
+        <div className="btn-secondary-3">
+          <div className="btn-text-master-6">
+            <p className="text-master-1 flex-vcenter-hcenter">Ver mais</p>
+          </div>
+        </div>
+      </div>
+      <div className="contents-05 flex-col-hcenter">
+        <p className="txt-3107">Histórico de consumo</p>
+        <div className="frame-611 flex-col-hcenter-vcenter">
+          <p className="txt-659 flex-hcenter">Hoje - 02/02/2022</p>
+          <div className="medicamento-21 flex-row-hsb">
+            <p className="txt-1080">Losartana - 50mg</p>
+            <p className="txt-428 flex-hend">10:02</p>
+          </div>
+          <p className="txt-729 flex-hcenter">Ontem - 01/02/2022</p>
+          <div className="medicamento-11 flex-row-hsb">
+            <p className="txt-494">Losartana - 50mg</p>
+            <p className="txt-799 flex-hend">22:05</p>
+          </div>
+          <div className="medicamento-31 flex-row-hsb">
+            <p className="txt-393">Perindopril - 04mg</p>
+            <p className="txt-363 flex-hend">20:15</p>
+          </div>
+        </div>
+        <div className="btn-secondary-4">
+          <div className="btn-text-master-7">
+            <p className="text-master-1 flex-vcenter-hcenter">Ver mais</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
